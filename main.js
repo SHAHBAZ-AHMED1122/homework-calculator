177dd4dab98c02b5437c16441773837dc072b594
SHAHBAZ-AHMED1122 <161025292+SHAHBAZ-AHMED1122@users.noreply.github.com>
2024-04-19 16:37:33 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        message: "enter your first your number",
+        type: "number",
+        name: "firstnumber",
+    },
+    { message: "enter your second number", type: "number", name: "secondnumber" },
+    {
+        message: "please choose your operator to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["multiplication", "addition", "subtraction", "division", "power"],
+    },
+]);
+if (answer.operator === "multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else if (answer.operator === "power") {
+    console.log(answer.firstnumber ** answer.secondnumber);
+}
+else
+    console.log("please select apropriate operation");
